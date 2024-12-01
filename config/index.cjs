22e0ba0976063fc91fe1cf1f06010509ed1979ac
博 <111757980+jyb040329@users.noreply.github.com>
2024-12-02 01:57:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '赵欣宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oPmR36tHElgM70ingzSaDaX9PWU8',
+      id: 'oPmR36vgqpEU6VJmD_Urw2XNT0-g',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'OlxGAPFBZDMCJ_40UPcKWPNnvjPGeXbVhklsiVYgnwE',
+      useTemplateId: 'rA6v60HGIfJxVIWhhhVNMhur5mckXoAEPYA-eLbgZgE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '7-11',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'OlxGAPFBZDMCJ_40UPcKWPNnvjPGeXbVhklsiVYgnwE',
+  CALLBACK_TEMPLATE_ID: 'rA6v60HGIfJxVIWhhhVNMhur5mckXoAEPYA-eLbgZgE',
 
   CALLBACK_USERS: [
     {
