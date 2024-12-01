ea581df8901036f699f80498178b011403656b4f
博 <111757980+jyb040329@users.noreply.github.com>
2024-12-02 01:29:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,9 +23,9 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oPmR36tHElgM70ingzSaDaX9PWU8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'NypxmhBCfmuas9P2vwaHyd2QMyIWPhIXmAwen_xkbfs',
+      useTemplateId: 'rA6v60HGIfJxVIWhhhVNMhur5mckXoAEPYA-eLbgZgE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '7-11',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'V9rAOWAW8xscNqCpUQ_Kdh8xqLwC1-cHcv0GgNyJDkI',
+  CALLBACK_TEMPLATE_ID: 'rA6v60HGIfJxVIWhhhVNMhur5mckXoAEPYA-eLbgZgE',
 
   CALLBACK_USERS: [
     {
